718cd99402922fa6fdb4701351338a5c725b8905
VasanthiKuppuswamy <Vasanthik@gmx.net>
2025-07-23 08:46:29 +0200

    Created Menu

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import products
 import store
 
+def start(store_object):
 
 
+    print("""Store Menu
+----------
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+Please choose a number: """)
 
 
-
-
-
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
+# setup initial stock of inventory
+product_list = [ products.Product("MacBook Air M2", price=1450, quantity=100),
+                 products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 products.Product("Google Pixel 7", price=500, quantity=250)
                ]
-
 best_buy = store.Store(product_list)
-products = best_buy.get_all_products()
-print(products)
-print(best_buy.get_total_quantity())
-print(best_buy.order([(products[0], 1), (products[1], 2)]))
+start(best_buy)
+
+
+
+
+
+
+
